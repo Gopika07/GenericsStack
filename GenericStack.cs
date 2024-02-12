5c333a49efb367a17fdf4bbea2425b477e295cbf
E.P Gopika <gopikaep07@gmail.com>
2024-02-12 16:48:34 +0530

    Add project files.

---
+++ GenericStack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsStack
+{
+    class GStack<T>
+    {
+        T[] genStack;
+        int top;
+        public GStack() 
+        {
+            genStack = new T[20];
+            top = -1;
+        }
+
+        public int Push(T val)
+        {
+            if (top == genStack.Length - 1)
+            {
+                return -1;
+            }
+            else
+            {
+                top++;
+                genStack[top] = val;
+            }
+            return 0;
+        }
+        public T Pop()
+        {
+            T val = default;
+            bool res = IsEmpty();
+            if (top >= 0)
+            {
+                val = genStack[top];
+                top--;
+            }
+            return val;
+        }
+
+        public T Peek()
+        {
+            return genStack[top];
+        }
+
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using GenericsStack;
+
+class Program
+{
+    static void Main()
+    {
+        GStack<int> stack = new GStack<int>();
+        int choice;
+        while (true)
+        {
+            Console.WriteLine("1. Push");
+            Console.WriteLine("2. Pop");
+            Console.WriteLine("3. Peek");
+            Console.WriteLine("4. Exit");
+
+            Console.WriteLine("Enter your Choice:");
+            choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter a value to push");
+                    int val = int.Parse(Console.ReadLine());
+                    stack.Push(val);
+                    break;
+                case 2:
+                    if (stack.IsEmpty())
+                    {
+                        Console.WriteLine("Stack is empty!");
+                    }
+                    else
+                    {
+                        int valPop = stack.Pop();
+                        Console.WriteLine("Value popped:" + valPop);
+                    }
+                    break;
+                case 3:
+                    if (stack.IsEmpty())
+                    {
+                        Console.WriteLine("Stack is empty!");
+                    }
+                    else
+                    {
+                        int valTop = stack.Peek();
+                        Console.WriteLine("Top most element is:" + valTop);
+                    }
+                    break;
+                case 4:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("You have Entered Wrong Choice");
+                    break;
+
+            }
+        }
+    }
+}
